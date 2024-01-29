44903aab077f73ab60b4b143c1659b8e3d697d3f
Mario <mario.rodcor@educa.jcyl.es>
2024-01-29 12:35:35 +0100

    4º commit

--- lib/paginas/nuevoMedicamento_1.dart
+++ lib/paginas/nuevoMedicamento_1.dart
@@ -7,82 +7,68 @@ class NuevoMedicamento_1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //##################### AppBar #####################
-      appBar: AppBar(),
-      //##################### Body #####################
-      body: Scrollbar(
-        child: Column(
-        children: [
-          const Expanded(child: Text("")),
-          const Align(
-            alignment: Alignment.center,
-            child: Padding(
-              padding: EdgeInsets.all(8),
-              child: Text("Nombre del medicamento"),
-            ),
-          ),
-          const Align(
-            alignment: Alignment.center,
-            child: Padding(
-              padding: EdgeInsets.all(8),
-              child: TextField(),
-            ),
-          ),
-          const Align(
-            alignment: Alignment.center,
-            child: Padding(
-              padding: EdgeInsets.all(8),
-              child: Text("Foto del medicamento"),
-            )
-          ),
-          const Align(
-            alignment: Alignment.center,
-            child: Padding(
-              padding: EdgeInsets.all(8),
-              child: SizedBox(
-                height: 10, 
-                width: 10,
-              ),
-            )
-          ),
-          Align(
-            alignment: Alignment.center,
-            child: Padding(
-              padding: const EdgeInsets.all(8),
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text("Importar foto") 
-              ),
-            )
-          ),
-          Align(
-            alignment: Alignment.center,
-            child: Padding(
-              padding: const EdgeInsets.all(8),
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text("Hacer foto") 
-              ),
-            )
-          ),
-          Expanded(
-            child: Align(
+        //##################### AppBar #####################
+        appBar: AppBar(),
+        //##################### Body #####################
+        body: SingleChildScrollView(
+            child: Column(
+          children: [
+            const Align(
               alignment: Alignment.center,
               child: Padding(
-                padding: const EdgeInsets.all(8),
-                child: ElevatedButton(
-                  onPressed: () {
-                    final destino = MaterialPageRoute(
-                                builder: (_) => const NuevoMedicamento_2());
-                            Navigator.push(context, destino);
-                  },
-                  child: const Text("Siguiente") 
-                ),
-              )
+                padding: EdgeInsets.all(8),
+                child: Text("Nombre del medicamento"),
+              ),
             ),
-          )
-        ],
-      ))
-    );
+            const Align(
+              alignment: Alignment.center,
+              child: Padding(
+                padding: EdgeInsets.all(8),
+                child: TextField(),
+              ),
+            ),
+            const Align(
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Text("Foto del medicamento"),
+                )),
+            const Align(
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: SizedBox(
+                    height: 10,
+                    width: 10,
+                  ),
+                )),
+            Align(
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: ElevatedButton(
+                      onPressed: () {}, child: const Text("Importar foto")),
+                )),
+            Align(
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: ElevatedButton(
+                      onPressed: () {}, child: const Text("Hacer foto")),
+                )),
+            Align(
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: ElevatedButton(
+                      onPressed: () {
+                        final destino = MaterialPageRoute(
+                            builder: (_) => const NuevoMedicamento_2());
+                        Navigator.push(context, destino);
+                      },
+                      child: const Text("Siguiente")),
+                )),
+          ],
+        )));
   }
 }
--- lib/paginas/nuevoMedicamento_2.dart
+++ lib/paginas/nuevoMedicamento_2.dart
@@ -9,197 +9,224 @@ class NuevoMedicamento_2 extends StatefulWidget {
 }
 
 class _NuevoMedicamento_2State extends State<NuevoMedicamento_2> {
+  bool? lunes = false;
+  bool? martes = false;
+  bool? miercoles = false;
+  bool? jueves = false;
+  bool? viernes = false;
+  bool? sabado = false;
+  bool? domingo = false;
+
   @override
   Widget build(BuildContext context) {
-    bool lunes = false;
-    bool martes = false;
-    bool miercoles = false;
-    bool jueves = false;
-    bool viernes = false;
-    bool sabado = false;
-    bool domingo = false;
-
     return Scaffold(
-      //##################### AppBar #####################
-      appBar: AppBar(),
-      //##################### Body #####################
-      body: Scrollbar(
-        child: Column(
-        children: [
-          const Expanded(child: Text("")),
-          const Align(
-            alignment: Alignment.center,
-            child: Padding(
-              padding: EdgeInsets.all(8),
-              child: Text("¿Que dias te tomas las pastillas?"),
-            ),
-          ),
-          Row(
-            children: [
-              const Text("Lunes"),
-              Align(
-                alignment: Alignment.center,
-                child: Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Checkbox(
-                    value: lunes, 
-                    onChanged: (bool? value) {
-                      setState(() {
-                        lunes = value!;
-                      });
-                    },
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              const Text("Martes"),
-              Align(
-                alignment: Alignment.center,
-                child: Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Checkbox(
-                    value: martes, 
-                    onChanged: (bool? value) {
-                      setState(() {
-                        martes = value!;
-                      });
-                    },
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              const Text("Miercoles"),
-              Align(
-                alignment: Alignment.center,
-                child: Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Checkbox(
-                    value: miercoles, 
-                    onChanged: (bool? value) {
-                      setState(() {
-                        miercoles = value!;
-                      });
-                    },
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              const Text("Jueves"),
-              Align(
-                alignment: Alignment.center,
-                child: Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Checkbox(
-                    value: jueves, 
-                    onChanged: (bool? value) {
-                      setState(() {
-                        jueves = value!;
-                      });
-                    },
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              const Text("Viernes"),
-              Align(
-                alignment: Alignment.center,
-                child: Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Checkbox(
-                    value: viernes, 
-                    onChanged: (bool? value) {
-                      setState(() {
-                        viernes = value!;
-                      });
-                    },
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              const Text("Sabado"),
-              Align(
-                alignment: Alignment.center,
-                child: Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Checkbox(
-                    value: sabado, 
-                    onChanged: (bool? value) {
-                      setState(() {
-                        sabado = value!;
-                      });
-                    },
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              const Text("Domingo"),
-              Align(
-                alignment: Alignment.center,
-                child: Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Checkbox(
-                    value: domingo, 
-                    onChanged: (bool? value) {
-                      setState(() {
-                        domingo = value!;
-                      });
-                    },
-                  ),
-                ),
-              ),
-            ],
-          ),
-          
-          const Align(
-            alignment: Alignment.center,
-            child: Padding(
-              padding: EdgeInsets.all(8),
-              child: Text("Hora a la que se\n toma el medicamento"),
-            )
-          ),
-          const Align(
-            alignment: Alignment.center,
-            child: Padding(
-              padding: EdgeInsets.all(8),
-              child: TextField()
-            )
-          ),
-          Expanded(
-            child: Align(
+        //##################### AppBar #####################
+        appBar: AppBar(),
+        //##################### Body #####################
+        body: SingleChildScrollView(
+            child: Column(
+          children: [
+            const Align(
               alignment: Alignment.center,
               child: Padding(
-                padding: const EdgeInsets.all(8),
-                child: ElevatedButton(
-                  onPressed: () {
-                    final destino = MaterialPageRoute(
-                                builder: (_) => const ListaMedicamentos());
-                            Navigator.push(context, destino);
-                  },
-                  child: const Text("Finalizar") 
-                ),
-              )
+                padding: EdgeInsets.all(8),
+                child: Text("¿Que dias te tomas las pastillas?"),
+              ),
             ),
-          )
-        ],
-      ))
-    );
+            Align(
+              alignment: Alignment.center,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 8, right: 8),
+                child: Container(
+                  decoration: const BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          topRight: Radius.circular(10)),
+                      border: Border(
+                          bottom: BorderSide(width: 2, color: Colors.black))),
+                  child: CheckboxListTile(
+                    title: const Text("Lunes"),
+                    value: lunes,
+                    onChanged: (value) {
+                      setState(() {
+                        lunes = value;
+                      });
+                    },
+                    activeColor: Colors.deepPurpleAccent,
+                    checkColor: Colors.white,
+                    controlAffinity: ListTileControlAffinity.leading,
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 8, right: 8),
+                child: Container(
+                  decoration: const BoxDecoration(
+                      border: Border(
+                          bottom: BorderSide(width: 2, color: Colors.black))),
+                  child: CheckboxListTile(
+                    title: const Text("Martes"),
+                    value: martes,
+                    onChanged: (value) {
+                      setState(() {
+                        martes = value;
+                      });
+                    },
+                    activeColor: Colors.deepPurpleAccent,
+                    checkColor: Colors.white,
+                    tileColor: Colors.blue,
+                    controlAffinity: ListTileControlAffinity.leading,
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 8, right: 8),
+                child: Container(
+                  decoration: const BoxDecoration(
+                      border: Border(
+                          bottom: BorderSide(width: 2, color: Colors.black))),
+                  child: CheckboxListTile(
+                    title: const Text("Miercoles"),
+                    value: miercoles,
+                    onChanged: (value) {
+                      setState(() {
+                        miercoles = value;
+                      });
+                    },
+                    activeColor: Colors.deepPurpleAccent,
+                    checkColor: Colors.white,
+                    tileColor: Colors.blue,
+                    controlAffinity: ListTileControlAffinity.leading,
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 8, right: 8),
+                child: Container(
+                  decoration: const BoxDecoration(
+                      border: Border(
+                          bottom: BorderSide(width: 2, color: Colors.black))),
+                  child: CheckboxListTile(
+                    title: const Text("Jueves"),
+                    value: jueves,
+                    onChanged: (value) {
+                      setState(() {
+                        jueves = value;
+                      });
+                    },
+                    activeColor: Colors.deepPurpleAccent,
+                    checkColor: Colors.white,
+                    tileColor: Colors.blue,
+                    controlAffinity: ListTileControlAffinity.leading,
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 8, right: 8),
+                child: Container(
+                  decoration: const BoxDecoration(
+                      border: Border(
+                          bottom: BorderSide(width: 2, color: Colors.black))),
+                  child: CheckboxListTile(
+                    title: const Text("Viernes"),
+                    value: viernes,
+                    onChanged: (value) {
+                      setState(() {
+                        viernes = value;
+                      });
+                    },
+                    activeColor: Colors.deepPurpleAccent,
+                    checkColor: Colors.white,
+                    tileColor: Colors.blue,
+                    controlAffinity: ListTileControlAffinity.leading,
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 8, right: 8),
+                child: Container(
+                  decoration: const BoxDecoration(
+                      border: Border(
+                          bottom: BorderSide(width: 2, color: Colors.black))),
+                  child: CheckboxListTile(
+                    title: const Text("Sabado"),
+                    value: sabado,
+                    onChanged: (value) {
+                      setState(() {
+                        sabado = value;
+                      });
+                    },
+                    activeColor: Colors.deepPurpleAccent,
+                    checkColor: Colors.white,
+                    tileColor: Colors.blue,
+                    controlAffinity: ListTileControlAffinity.leading,
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 8, right: 8),
+                child: Container(
+                  decoration: const BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(10),
+                          bottomRight: Radius.circular(10))),
+                  child: CheckboxListTile(
+                    title: const Text("Domingo"),
+                    value: domingo,
+                    onChanged: (value) {
+                      setState(() {
+                        domingo = value;
+                      });
+                    },
+                    activeColor: Colors.deepPurpleAccent,
+                    checkColor: Colors.white,
+                    controlAffinity: ListTileControlAffinity.leading,
+                  ),
+                ),
+              ),
+            ),
+            const Align(
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Text("Hora a la que se\n toma el medicamento"),
+                )),
+            const Align(
+                alignment: Alignment.center,
+                child: Padding(padding: EdgeInsets.all(8), child: TextField())),
+            Align(
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: ElevatedButton(
+                      onPressed: () {
+                        final destino = MaterialPageRoute(
+                            builder: (_) => const ListaMedicamentos());
+                        Navigator.push(context, destino);
+                      },
+                      child: const Text("Finalizar")),
+                )),
+          ],
+        )));
   }
 }
